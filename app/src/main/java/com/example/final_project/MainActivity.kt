f211cd84084270e6c76f79a1c5073cc7583ed853
Jamey-UCWV <92441905+Jamey-UCWV@users.noreply.github.com>
2022-04-20 19:33:27 -0400

    Finished coding each fragment but now facing a problem where the app constantly stops upon running. No errors in the build, unsure of how to fix it.

--- app/src/main/java/com/example/final_project/MainActivity.kt
+++ app/src/main/java/com/example/final_project/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.final_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import com.example.final_project.fragments.FlightFragment
+import com.example.final_project.fragments.HomeFragment
+import com.example.final_project.fragments.ScorecardFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+        private val homeFragment = HomeFragment()
+        private val flightFragment = FlightFragment()
+        private val scorecardFragment = ScorecardFragment()
+        private val bottom_navigation=findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        replaceFragment(homeFragment)
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.ic_home -> replaceFragment(homeFragment)
+                R.id.ic_flight_path -> replaceFragment(flightFragment)
+                R.id.ic_scorecard -> replaceFragment(scorecardFragment)
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        if(fragment !=null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, fragment)
+            transaction.commit()
+
+
+        }
     }
 }
